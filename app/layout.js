c787bfffe635119dbe77a9ba14eb5a2022cb5885
vitorfarinha <35097861+vitorfarinha@users.noreply.github.com>
2025-11-20 11:47:39 +0000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -9,11 +9,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt">
-  <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-      </head>
       <body>
         {children}
       </body>
